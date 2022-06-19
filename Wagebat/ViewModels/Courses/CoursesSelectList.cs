37280bfbd32159f5b6b2602325fb31c223154bd9
mohamedriiyad <mohamedriiyad@gmail.com>
2022-06-19 19:17:18 +0200

    UpdateQuestionCreation

---
+++ Wagebat/ViewModels/Courses/CoursesSelectList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wagebat.ViewModels.Courses
+{
+    public class CoursesSelectList
+    {
+        public int UniversityId { get; set; }
+        public int LevelId { get; set; }
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+    }
+}
